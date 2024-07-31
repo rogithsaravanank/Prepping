1e3b45ce416cec71292595778db28757816b80b6
Rogith Saravanan <rogith27@gmail.com>
2024-07-31 20:39:50 +0530

    adding notes

---
+++ Prepping/src/main/java/org/example/Basics.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Basics {
+
+   static int num1=0x7E;
+   static int num2= 0b101;
+
+    public static void main(String[] args) {
+        System.out.println("Hello...");
+        System.out.println(num1+ " "+ num2);
+    }
+    
+}
