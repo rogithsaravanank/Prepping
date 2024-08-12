e668e1abf7fcb3f005b43fc41d098c9f308670c5
rkumar696 <rogithsaravanan_kumarasamy@comcast.com>
2024-08-12 20:07:16 +0530

    adding notes

---
+++ Prepping/src/main/java/org/example/JavaEight.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class JavaEight {
+    public static void main(String[] args) {
+        System.out.println("Getting into Java 8...");
+
+    }
+
+    public static void BasicStreams(){
+        
+    }
+}
